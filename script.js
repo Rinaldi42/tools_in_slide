ad3fba8f958f4e6e63368367b5e5a16d8a7f4a9d
GuilhermeRinaldi <guilherme.rinaldi@outlook.com>
2022-01-05 20:56:23 -0300

    final

--- script.js
+++ script.js
@@ -3,67 +3,96 @@ setInterval(function (){
 	document.getElementById('clock').innerHTML = data.getHours() + ':' + data.getMinutes() +  ':' + data.getSeconds();
 }, 100);
 
-let startBlock = true;
+function writeTime (id, hour, minute, second) {
+	document.getElementById(id).innerHTML = hour + ':' + minute + ':' + second;
+}
 
-function startStopwatch() {
-	let time = document.getElementById('stopwatch').innerHTML;
-	let hour = parseInt(time.split(":")[0]);
-	let minute = parseInt(time.split(":")[1]);
-	let second = parseInt(time.split(":")[2]); 
-	if (startBlock) {
-		startBlock = false;
-		interval = setInterval(function() {
-			second +=1;
-			if (second === 60) {
-				second = 0;
-				minute +=1;
-			}
-			if (minute === 60) {
-				minute = 0;
-				hour +=1;
-			}
-			if (second < 10) {
-				seconds = "0" + second;
+function write(id, value) {
+	document.getElementById(id).innerHTML = value;
+}
+
+let btnStopwatch = true;
+let btnHourglass = true;
+
+let timeStopwatch = 1;
+let timeHourglass = 1;
+
+function stopwatch() {
+	let hour = ["0", Math.floor(timeStopwatch/3600)];
+	let minute = ["0", Math.floor(timeStopwatch/60)];
+	let second = ["0", timeStopwatch - minute[1]*60 - hour[1]*3600]; 
+	if (btnStopwatch) {
+		btnStopwatch = false;
+		intervalStopwatch = setInterval(function() {
+			hour[1] = Math.floor(timeStopwatch/3600) ;
+			minute[1] = Math.floor(timeStopwatch/60) - hour[1]*60 ;
+
+			hour = hour >= 10 ? ["", hour[1]] : ["0", hour[1]];
+			minute = minute >= 10 ? ["", minute[1]] : ["0", minute[1]];
+
+			if (timeStopwatch%60 != 0) {
+				second[1] = timeStopwatch - minute[1]*60 - hour[1]*3600;
 			}else {
-				seconds = second;
+				second[1] = 0;
 			}
-			if (minute < 10) {
-				minutes = "0" + minute;
-			}else {
-				minutess = minute;
-			}
-			if (hour < 10) {
-				hours = "0" + hour;
-			}else {
-				hours = hour;
-			}
-			document.getElementById('stopwatch').innerHTML = hours+ ':' + minutes + ':' + seconds;
+			second = second[1] >= 10 ? ["", second[1]] : ["0", second[1]];
+			writeTime('stopwatch', hour[0]+ hour[1], minute[0] + minute[1], second[0] + second[1]);
+			timeStopwatch += 1;
 		}, 1000);
 	}
 }
 
-function stop() {
-	clearInterval(interval);
-	startBlock = true;
+function hourglass() {
+	let hour = ["0", parseInt(document.getElementById('hour').value)];
+	let minute = ["0", parseInt(document.getElementById('minute').value)];
+	let second = ["0", parseInt(document.getElementById('second').value)]; 
+	let time = hour[1]*3600 + minute[1]*60 + second[1];
+	if (btnHourglass) {
+		btnHourglass = false;
+		intervalHourglass = setInterval(function() {
+			timeleft = time - timeHourglass;
+			hour[1] = Math.floor(timeleft/3600);
+			minute[1] = Math.floor(timeleft/60) - hour[1]*60;
+			second[1] = timeleft - minute[1]*60 - hour[1]*3600;
+			hour = hour >= 10 ? ["", hour[1]] : ["0", hour[1]];
+			minute = minute >= 10 ? ["", minute[1]] : ["0", minute[1]];
+			second = second[1] >= 10 ? ["", second[1]] : ["0", second[1]];
+			writeTime('hourglass', hour[0]+ hour[1], minute[0] + minute[1], second[0] + second[1]);
+			timeHourglass += 1;
+		}, 1000);
+	}
 }
 
-function reset() {
-	clearInterval(interval);
-	startBlock = true
-	document.getElementById('stopwatch').innerHTML = '00:00:00';
+function stopStopwatch() {
+	clearInterval(intervalStopwatch);
+	btnStopwatch = true;
+}
+
+
+function resetStopwatch() {
+	timeStopwatch = 1;
+	clearInterval(intervalStopwatch);
+	btnStopwatch = true
+}
+
+function resetHourglass() {
+	timeHourglass = 1;
+	clearInterval(intervalHourglass);
+	btnHourglass = true
+	writeTime('hourglass',"00","00","00");
 
 }
 
 function winner() {
 	const competitors = document.getElementById('competitors').value;
 	const winner = Math.floor(Math.random() * (competitors - 1) + 1);
-	document.getElementById('winner').innerHTML = winner;
+	write('winner', winner);
 }
   
 function password() {
 	const down = "abcdefghijklmnopqrstuvwxyz";
 	const up = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	const esp = "!@()+-={}~"
+	const esp = "!'#$%&()*+,-./:;<=>?@[]^_{}";
 	let password = "";
 	let length = 26
 	for (var i = 0; i <= 10; i++) {
@@ -71,21 +100,18 @@ function password() {
 		if (i <= 3) {
 			password += down[num];
 		}
-		if (i > 4 & i <= 6) {
+		if (i > 3 & i <= 5) {
 			password += up[num];
 		}
-		if (i > 6 & i <=9) {
-			length = 10;
-			if (i > 7) {
-				password += esp[num];	
-			}
+		if (i > 6 & i <=8) {
+			password += esp[num];	 
 		}
-		if (i > 8 & i <=10) {
+		if (i > 9 & i <=10) {
 			password += num;
 		}
 
 	}
-	document.getElementById('password').value = password;
+	write('password', password);
 }
 
 function copy() {
